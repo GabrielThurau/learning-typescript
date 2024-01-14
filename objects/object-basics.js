4c71bc5e8e585ba02626fce0b8f249e92fab08cd
GabrielThurau <gjthurau92@yahoo.com>
2024-01-14 09:31:27 -0700

    stuff

--- objects/object-basics.js
+++ objects/object-basics.js
@@ -1,9 +1,31 @@
+// function printName(person: { first: string, last: string }) {
+//     console.log(`${person.first} ${person.last}`)
+// }
 function printName(person) {
-    console.log("".concat(person.first, " ").concat(person.last));
+    return { first: person.first, last: person.last, age: person.age };
 }
-printName({ first: "Gabe", last: "Thurau" });
-// object shape
+printName({ first: "Hank", last: "Hill", age: 100 });
 var coordinate = { x: 34, y: 42 };
 function randomCoordinate() {
     return { x: Math.random(), y: Math.random() };
 }
+function doublePoint(point) {
+    return { x: point.x * 2, y: point.y * 2 };
+}
+function calculatePayout(song) {
+    return song.numStreams * 0.0033;
+}
+function printSong(song) {
+    console.log("".concat(song.title, " by ").concat(song.artist, " \nhas ").concat(song.numStreams, " total streams. The album this song is from is ").concat(song.albumInfo.albumName, " \nand it was released in ").concat(song.albumInfo.yearReleased, " "));
+}
+var theSong = {
+    title: "What a Horrible Night to Have a Curse",
+    artist: "The Black Dahlia Murder",
+    numStreams: 1000000000,
+    albumInfo: {
+        recordLabel: "Sumerian Records",
+        yearReleased: 2007,
+        albumName: "Nocturnal"
+    }
+};
+printSong(theSong);
--- objects/object-basics.ts
+++ objects/object-basics.ts
@@ -1,14 +1,73 @@
-function printName(person: { first: string, last: string }) {
-    console.log(`${person.first} ${person.last}`)
+// function printName(person: { first: string, last: string }) {
+//     console.log(`${person.first} ${person.last}`)
+// }
+
+// printName({ first: "Gabe", last: "Thurau" })
+
+
+// object shape + type alias "Point"
+
+
+type Point = {
+    x: number,
+    y: number,
+    z?: number
 }
 
-printName({ first: "Gabe", last: "Thurau" })
+// ? in front of z makes it an optional property
 
+type Person = {
+    first: string,
+    last: string,
+    age: number
+}
 
-// object shape
+function printName(person: Person): Person {
+    return { first: person.first, last: person.last, age: person.age }
+}
 
-let coordinate: { x: number; y: number } = { x: 34, y: 42 }
+printName({ first: "Hank", last: "Hill", age: 100 })
 
-function randomCoordinate(): { x: number; y: number } {
+let coordinate: Point = { x: 34, y: 42 }
+
+function randomCoordinate(): Point {
     return { x: Math.random(), y: Math.random() };
 }
+
+function doublePoint(point: Point): Point {
+    return { x: point.x * 2, y: point.y * 2 }
+}
+
+// nested object types
+
+type Song = {
+    title: string,
+    artist: string,
+    numStreams: number,
+    albumInfo: { recordLabel: string, yearReleased: number, albumName: string }
+}
+
+function calculatePayout(song: Song): number {
+    return song.numStreams * 0.0033;
+}
+
+function printSong(song: Song): void {
+    console.log(`${song.title} by ${song.artist} 
+has ${song.numStreams} total streams. The album this song is from is ${song.albumInfo.albumName} 
+and it was released in ${song.albumInfo.yearReleased} `)
+}
+
+const theSong: Song = {
+    title: "What a Horrible Night to Have a Curse",
+    artist: "The Black Dahlia Murder",
+    numStreams: 1000000000,
+    albumInfo: {
+        recordLabel: "Sumerian Records",
+        yearReleased: 2007,
+        albumName: "Nocturnal"
+    }
+
+}
+
+printSong(theSong)
+
